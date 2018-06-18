22bf78b8bf71cae73955e3e3881bca65cca6b51d
majikandy <github@andy-davis.co.uk>
2018-06-18 10:22:38 +0100

    expanded disclaimer

--- WorldCupSweepstake/Sweepstake.cs
+++ WorldCupSweepstake/Sweepstake.cs
@@ -1,7 +1,8 @@
 ﻿using Stratis.SmartContracts;
 
 /// <summary>
-/// DISCLAIMER: This contract is an experimental bit of fun for the world cup and NOT reference code
+/// DISCLAIMER: This contract is an experimental bit of fun for the world cup and NOT reference code. 
+/// It is not guranteed to work and is my own work, not the work of the company.
 /// </summary>
 public class Sweepstake : SmartContract
 {
